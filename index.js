1d367f10a6e255c57ee5b352259f6036ac8fdae6
ChitikelaDurgaPrasad <144230229+ChitikelaDurgaPrasad@users.noreply.github.com>
2024-01-09 10:40:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,13 +19,20 @@
 
 // first dicee 
 
-let dicee2=0, dicee1=0;
+let dicee2=0, dicee1=0, to_load=0,count=0;
+document.querySelector("button").addEventListener("click", first_dice);
 function first_dice(){
     dicee1 = Math.floor((Math.random()*6)+1);
     let dicee1_image ="./images/dice"+dicee1+".png";
     document.querySelectorAll("img")[0].setAttribute("src",dicee1_image);
-    document.querySelectorAll("button")[0].classList.add("btn");
-    if(dicee2!==0){  //if(dicee2>=1 && dicee2s<=6);
+    count=count+dicee1;
+    if(dicee1!==6){
+        document.querySelectorAll("button")[0].classList.add("btn");
+        dicee1=count;
+        count=0;
+        document.querySelectorAll("button")[1].addEventListener("click", second_dice);
+    }
+    if(dicee2!==0 && count===0){  //if(dicee2>=1 && dicee2s<=6);
         Game();
     }
 }
@@ -33,19 +40,33 @@ function second_dice(){
     dicee2 = Math.floor((Math.random()*6)+1);
     let dicee2_image ="./images/dice"+dicee2+".png";
     document.querySelectorAll("img")[1].setAttribute("src",dicee2_image);
-    document.querySelectorAll("button")[1].classList.add("btn");
-    if(dicee1!==0){    //if(dicee1>=1 && dicee1<=6);
+    count=count+dicee2;
+    if(dicee2!==6){
+        document.querySelectorAll("button")[1].classList.add("btn");
+        dicee2=count;
+        count=0;
+    }
+    if(dicee1!==0 && count===0){    //if(dicee1>=1 && dicee1<=6);
         Game();
     }
 }
 function Game(){
     if(dicee1 > dicee2){
-        document.querySelector("h1").textContent = "player 1 wins!";  
+        document.querySelector("h1").textContent = "player 1 wins!";
+        to_load++;
     }
     else if (dicee1<dicee2){
         document.querySelector("h1").textContent = "player 2 wins!";
+        to_load++;
     }
     else {
         document.querySelector("h1").textContent = "Draw!";
+        to_load++;
+    }
+}
+
+function load(){
+    if(to_load===1){
+        location.reload();
     }
 }
